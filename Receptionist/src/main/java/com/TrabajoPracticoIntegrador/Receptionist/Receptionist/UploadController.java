f435b6f20e9f7593c19e814ade06adca2645d262
Agustin Normand <NormandAgustin@gmail.com>
2021-07-12 13:33:37 -0300

    Pruebo CICD

--- Receptionist/src/main/java/com/TrabajoPracticoIntegrador/Receptionist/Receptionist/UploadController.java
+++ Receptionist/src/main/java/com/TrabajoPracticoIntegrador/Receptionist/Receptionist/UploadController.java
@@ -25,7 +25,7 @@ public class UploadController {
 
     @GetMapping("/")
     public String index(){
-        return "IndexV1";
+        return "IndexV2";
     }
 
     @PostMapping("/upload") // //new annotation since 4.3
