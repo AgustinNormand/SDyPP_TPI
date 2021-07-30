d9faedb9d4455bc0420a4dabedce49a79f947bbb
marianorapa <marianorapaport@gmail.com>
2021-07-30 00:29:07 -0300

    move ResultStatus

--- management-app/cluster-applier/src/main/java/com/example/clusterApplier/core/enums/ResultStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.clusterApplier.core.enums;
-
-public enum ResultStatus {
-    OK, ERROR
-}
---
+++ management-app/commons/src/main/java/com/example/commons/dto/enums/ResultStatus.java
@@ -0,0 +1,5 @@
+package com.example.commons.dto.enums;
+
+public enum ResultStatus {
+    OK, ERROR
+}
