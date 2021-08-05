f1820a66fb0acb241596f66e8c09611e79cfbb54
Agustin Normand <NormandAgustin@gmail.com>
2021-08-05 19:57:04 -0300

    Elimino redis de resources

---
+++ Terraform/get_credentials.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+get_value () {
+    echo $(cat variables.tf | grep $1 -A 4 | grep default | awk {'print $3'} | sed 's/"//g') 
+}
+
+gcloud container clusters get-credentials $(get_value "management_cluster_name") --region $(get_value "management_region")
+
+gcloud container clusters get-credentials $(get_value "deployments_cluster_name") --region $(get_value "deployments_region")
+
+gcloud container clusters get-credentials $(get_value "resources_cluster_name") --region $(get_value "resources_region")
