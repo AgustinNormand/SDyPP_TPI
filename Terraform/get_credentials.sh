5bb7cf051241323ee23ee03c20a3c5e7d2837a63
Agustin Normand <NormandAgustin@gmail.com>
2021-08-11 16:53:42 -0300

    add credentials

--- Terraform/get_credentials.sh
+++ Terraform/get_credentials.sh
@@ -4,6 +4,10 @@ get_value () {
     echo $(cat variables.tf | grep $1 -A 4 | grep default | awk {'print $3'} | sed 's/"//g') 
 }
 
+gcloud iam service-accounts keys create ./credentials.json --iam-account=$(get_service_account_name $(get_value "management_nodes_sa_name"))
+
+gcloud auth activate-service-account $(get_service_account_name $(get_value "management_nodes_sa_name")) --key-file=./credentials.json
+
 rm ~/.kube/config
 
 gcloud container clusters get-credentials $(get_value "management_cluster_name") --region $(get_value "management_region")
