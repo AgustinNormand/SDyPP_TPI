44072dfb70449d2c1d62dac1c4422f32f110c62f
Agustin Normand <NormandAgustin@gmail.com>
2021-08-11 16:49:31 -0300

    add rm kubeconfig

--- Terraform/get_credentials.sh
+++ Terraform/get_credentials.sh
@@ -4,6 +4,8 @@ get_value () {
     echo $(cat variables.tf | grep $1 -A 4 | grep default | awk {'print $3'} | sed 's/"//g') 
 }
 
+rm ~/.kube/config
+
 gcloud container clusters get-credentials $(get_value "management_cluster_name") --region $(get_value "management_region")
 
 gcloud container clusters get-credentials $(get_value "deployments_cluster_name") --region $(get_value "deployments_region")
