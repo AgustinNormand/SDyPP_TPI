2bb14abb0998701f3abe7935b196f8cd460e9706
Agustin Normand <NormandAgustin@gmail.com>
2021-08-13 10:42:39 -0300

    fix destroy.sh

--- Terraform/destroy.sh
+++ Terraform/destroy.sh
@@ -2,7 +2,7 @@ get_value () {
     echo $(cat variables.tf | grep $1 -A 4 | grep default | awk {'print $3'} | sed 's/"//g') 
 }
 
-RECORD_SETS=$(gcloud dns record-sets list --zone $(get_value "framework-services-gcp-com-ar") | tail -n +2 | awk {'print $1"&"$2'})
+RECORD_SETS=$(gcloud dns record-sets list --zone $(get_value "dns_zone_name") | tail -n +2 | awk {'print $1"&"$2'})
 
 for RECORD_SET in $RECORD_SETS ; do
     RECORD_ARRAY=(${RECORD_SET//&/ })
@@ -10,8 +10,8 @@ for RECORD_SET in $RECORD_SETS ; do
     TYPE=${RECORD_ARRAY[1]}
     if [ $TYPE == A ] || [ $TYPE == TXT ]
     then
-        gcloud dns record-sets delete $RECORD --zone $(get_value "framework-services-gcp-com-ar") --type $TYPE
+        gcloud dns record-sets delete $RECORD --zone $(get_value "dns_zone_name") --type $TYPE
     fi
 done
 
-gcloud dns managed-zones delete $(get_value "framework-services-gcp-com-ar")
+gcloud dns managed-zones delete $(get_value "dns_zone_name")
