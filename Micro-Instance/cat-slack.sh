2cf2e6016b15155d18d6f5929a4848b960307ce7
Agustin Normand <NormandAgustin@gmail.com>
2021-08-18 13:42:30 -0300

    remove webhook

--- Micro-Instance/cat-slack.sh
+++ Micro-Instance/cat-slack.sh
@@ -2,5 +2,5 @@
 
 cat "$1" | while read LINE; do
   (echo "$LINE" | grep -e "$3") && curl -X POST --silent --data-urlencode \
-    "payload={\"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" "https://hooks.slack.com/services/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";
+    "payload={\"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" "$2";
 done
