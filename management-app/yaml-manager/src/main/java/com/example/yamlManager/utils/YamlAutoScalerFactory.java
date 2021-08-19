153b362b49fb58114aefca0d787782dab1f322c9
marianorapa <marianorapaport@gmail.com>
2021-08-18 21:48:52 -0300

    read template from classpath

--- management-app/yaml-manager/src/main/java/com/example/yamlManager/utils/YamlAutoScalerFactory.java
+++ management-app/yaml-manager/src/main/java/com/example/yamlManager/utils/YamlAutoScalerFactory.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.utils.SerializationUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
@@ -34,7 +35,8 @@ public class YamlAutoScalerFactory {
         try {
             logger.debug("Creating autoscaler");
 
-            String template = Files.readString(Path.of("src/main/resources/templates/autoscaler.yaml"));
+            String template = Files
+                    .readString(ResourceUtils.getFile("classpath:templates/autoscaler.yaml").toPath());
 
             logger.debug("Template loaded: {}", template);
 
